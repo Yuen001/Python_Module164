2cf0d2517fff609d85a586184f195e99c800923b
cfcnibt <cfcnibt@proton.me>
2024-05-29 00:08:37 +0200

    Changements pour EDIT. OM

--- APP_FILMS_164/genres/gestion_genres_crud.py
+++ APP_FILMS_164/genres/gestion_genres_crud.py
@@ -34,14 +34,13 @@ def genres_afficher(order_by, id_genre_sel):
         try:
             with DBconnection() as mc_afficher:
                 if order_by == "ASC" and id_genre_sel == 0:
-                 strsql_genres_afficher = """SELECT * FROM t_object ORDER BY id_object ASC"""
-                 mc_afficher.execute(strsql_genres_afficher)
+                    strsql_genres_afficher = """SELECT * FROM t_object ORDER BY id_object ASC"""
+                    mc_afficher.execute(strsql_genres_afficher)
                 elif order_by == "ASC":
                     # C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
                     # la commande MySql classique est "SELECT * FROM t_genre"
                     # Pour "lever"(raise) une erreur s'il y a des erreurs sur les noms d'attributs dans la table
                     # donc, je précise les champs à afficher
-                    mc_afficher.execute(strsql_genres_afficher)
                     # Constitution d'un dictionnaire pour associer l'id du genre sélectionné avec un nom de variable
                     valeur_id_genre_selected_dictionnaire = {"value_id_genre_selected": id_genre_sel}
                     strsql_genres_afficher = """SELECT * FROM t_object WHERE id_object = %(value_id_genre_selected)s"""
@@ -155,7 +154,7 @@ def genres_ajouter_wtf():
 @app.route("/genre_update", methods=['GET', 'POST'])
 def genre_update_wtf(gouts_update_wtf=None):
     # L'utilisateur vient de cliquer sur le bouton "EDIT". Récupère la valeur de "id_genre"
-    id_genre_update = request.values['id_genre_btn_edit_html']
+    id_object_update = request.values['id_genre_btn_edit_html']
 
     # Objet formulaire pour l'UPDATE
     form_update = FormWTFUpdateGenre()
@@ -167,18 +166,16 @@ def genre_update_wtf(gouts_update_wtf=None):
             # Puis la convertir en lettres minuscules.
             name_object_update = form_update.nom_genre_update_wtf.data
             name_object_update = name_object_update.lower()
-            date_genre_essai = form_update.gouts_update_wtf.data
+            gouts_update = form_update.gouts_update_wtf.data
 
-            valeur_update_dictionnaire = {"value_id_genre": id_genre_update,
+            valeur_update_dictionnaire = {"value_id_object": id_object_update,
                                           "value_name_object": name_object_update,
-                                          # "value_name_genre": name_genre_update
-                                          "value_gouts:object" : gouts_update_wtf,
-                                          #"value_date_genre_essai": date_genre_essai
+                                          "value_gouts_object" : gouts_update,
                                           }
             print("valeur_update_dictionnaire ", valeur_update_dictionnaire)
 
-            str_sql_update_intitulegenre = """UPDATE t_object SET nom_object = %(value_name_genre)s, 
-            date_ins_genre = %(value_date_genre_essai)s WHERE id_genre = %(value_id_genre)s """
+            str_sql_update_intitulegenre = """UPDATE t_object SET nom_object = %(value_name_object)s, 
+            Gouts = %(value_gouts_object)s WHERE ID_object = %(value_id_object)s """
             with DBconnection() as mconn_bd:
                 mconn_bd.execute(str_sql_update_intitulegenre, valeur_update_dictionnaire)
 
@@ -186,23 +183,23 @@ def genre_update_wtf(gouts_update_wtf=None):
             print(f"Donnée mise à jour !!")
 
             # afficher et constater que la donnée est mise à jour.
-            # Affiche seulement la valeur modifiée, "ASC" et l'"id_genre_update"
-            return redirect(url_for('genres_afficher', order_by="ASC", id_genre_sel=id_genre_update))
+            # Affiche seulement la valeur modifiée, "ASC" et l'"id_object_update"
+            return redirect(url_for('genres_afficher', order_by="ASC", id_genre_sel=id_object_update))
         elif request.method == "GET":
             # Opération sur la BD pour récupérer "id_genre" et "intitule_genre" de la "t_genre"
-            str_sql_id_genre = "SELECT ID_object, nom_object, Gouts FROM t_object " \
+            str_sql_id_genre = "SELECT * FROM t_object " \
                                "WHERE ID_object = %(value_ID_object)s"
-            valeur_select_dictionnaire = {"value_ID_object": id_genre_update}
+            valeur_select_dictionnaire = {"value_ID_object": id_object_update}
             with DBconnection() as mybd_conn:
                 mybd_conn.execute(str_sql_id_genre, valeur_select_dictionnaire)
             # Une seule valeur est suffisante "fetchone()", vu qu'il n'y a qu'un seul champ "nom genre" pour l'UPDATE
             data_nom_genre = mybd_conn.fetchone()
             print("gouts ", data_nom_genre, " type ", type(data_nom_genre), " gouts ",
-                  data_nom_genre["gouts"])
+                  data_nom_genre["Gouts"])
 
             # Afficher la valeur sélectionnée dans les champs du formulaire "genre_update_wtf.html"
             form_update.nom_genre_update_wtf.data = data_nom_genre["nom_object"]
-            form_update.gouts_update_wtf.data = data_nom_genre["date_ins_genre"]
+            form_update.gouts_update_wtf.data = data_nom_genre["Gouts"]
 
     except Exception as Exception_genre_update_wtf:
         raise ExceptionGenreUpdateWtf(f"fichier : {Path(__file__).name}  ;  "
